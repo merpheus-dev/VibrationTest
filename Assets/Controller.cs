d4de8eb7ca2b205359347dddda5060243f2483f9
m3rt32 <mertkirimgeri@gmail.com>
2018-10-29 23:15:38 +0300

    Initial source

---
+++ Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public void Peek()
+    {
+        Vibration.VibratePeek();
+    }
+
+
+    public void Pop()
+    {
+        Vibration.VibratePop();
+    }
+
+    public void Nope()
+    {
+        Vibration.VibrateNope();
+    }
+
+    public void Standart()
+    {
+        Vibration.Vibrate();
+    }
+
+}
